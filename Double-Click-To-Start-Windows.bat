53131146dd576a09bbe192c90a114853b3eda6a8
ajaydhavane <ajayd92@uw.edu>
2026-03-08 01:29:23 -0500

    Enhance user prompts and installation feedback
    Update installation message and improve user prompts.

--- Double-Click-To-Start-Windows.bat
+++ Double-Click-To-Start-Windows.bat
@@ -3,13 +3,13 @@ cd /d "%~dp0"
 
 where uv >nul 2>&1
 if %errorlevel% neq 0 (
-	powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"
+	echo Installing dependencies...
+	powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex" >nul 2>&1
 )
 
-echo Do not close this window until you are done using the app.
+echo.
+powershell -Command "Write-Host 'Do not close this window until you are done using the app!!!' -ForegroundColor Red"
+echo.
 
 uv run streamlit run src/main.py >nul
-
-echo.
-echo Process finished. You can close this window.
 pause
